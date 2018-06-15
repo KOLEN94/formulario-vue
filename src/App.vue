f959bb9f7c39f46fecb8f131a1dfaef25ad6f928
KOLLEN94 <breaknius@gmail.com>
2018-06-15 11:33:29 -0500

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+ #app
+    img(src="https://static.platzi.com/media/user_upload/Page%201-d8d869f4-7130-4ae9-9907-2fa809bd136a.jpg")
+    h1 {{ msg }}
+    form(id=form-platzi)
+      div
+        label(for="nombre") Nombre
+        input(type="text" name="nombre" id="nombre")
+      div
+        label(for="apellidos") Apellidos
+        input(type="text" name="apellidos" id="apellidos")
+      div
+        label(for="cargo") Cargo
+        input(type="text" name="cargo" id="cargo")
+      div
+        button(type="submit" value="enviar") Enviar
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss';
+</style>
